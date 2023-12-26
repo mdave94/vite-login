106032aa86bba6d4630039e9d3c001c4da10b128
Molnar Dávid <molnardavid2002@gmail.com>
2023-12-26 14:55:24 +0100

    custombutton is added

--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -1,16 +1,27 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import FormInput from "./UI/FormInput";
+import CustomButton from "./UI/CustomButton";
 
 const LoginForm = () => {
   const [name, setName] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!name.trim()) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="w-[450px] h-[450px] border rounded-lg dark:bg-gray-800 dark:border-gray-600 bg-white ">
           <div className="text-3xl text-black m-8 font-bold text-left dark:text-gray-200">
             Sign in
@@ -27,6 +38,7 @@ const LoginForm = () => {
                 onChange={handleChange}
               />
             </div>
+
             <div className="mb-8">
               <FormInput
                 value={name}
@@ -39,6 +51,9 @@ const LoginForm = () => {
               />
             </div>
           </div>
+          {error && <p className="text-red-500"> cannot be empty </p>}
+
+          <CustomButton title="Log in" />
         </div>
       </form>
     </>
